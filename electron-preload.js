7bf707211e22fb7671dbfd917752edca51a74758
Santiaxelpro <losnuevoslusax@gmail.com>
2026-02-06 15:42:02 -0500

    fix page not loading

--- electron-preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Exponer APIs seguras al renderer
-contextBridge.exposeInMainWorld('electron', {
-  platform: process.platform,
-  version: process.version
-});
